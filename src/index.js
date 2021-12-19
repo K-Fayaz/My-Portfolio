c76db29377e26da723aa5bbda0fa6b5a10e32fdf
Fayaz <kfayaz1407@gmail.com>
2021-12-19 13:06:45 +0530

    new Thing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NavBar from './NavBar';
+import Mainbody from './MainBody';
+import Skill from './Skill';
+import Projects from './Projects';
+import Footer from './Footer';
+import Email from './Email';
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <>
+    <NavBar />
+    <Mainbody/>
+    <Skill/>
+    <Projects/>
+    <Email/>
+    <Footer/>
+  </>,document.getElementById('root'));
